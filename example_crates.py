1bae4b5fc40ab4d23752bda20f92bbac7a5fb48d
Sebastian Holzapfel <schnommus@gmail.com>
2013-08-01 07:47:54 +1000

    Refactored Respawnable out of Step in 'crates demo'

--- example_crates.py
+++ example_crates.py
@@ -4,25 +4,21 @@ import math
 
 appSize = Vec2(800, 600)
 
-class Crate(ComponentEntity):
+class Respawnable(ComponentEntity):
+    def Step(self):
+        if self.position.y > appSize.y+100: # If object is offscreen
+            self.core.entityManager.AddEntity( type(self)() ) # Spawn new object
+            self.core.entityManager.RemoveEntity(self) # Destroy self
+
+class Crate(Respawnable):
     def Build(self):
         self.AddComponent( SpriteComponent( self.core.resourceManager.FetchTexture("media\\crate.png") ) )
         self.AddComponent( RigidBody_Rectangular((16, 16), (random.randint(200,appSize.x-200), (random.randint(-400, -100))) ) )
 
-    def Step(self):
-        if self.position.y > appSize.y+100: # If object is offscreen
-            self.core.entityManager.AddEntity( Crate() ) # Spawn new object
-            self.core.entityManager.RemoveEntity(self) # Destroy self
-
-class Stone(ComponentEntity):
+class Stone(Respawnable):
     def Build(self):
         self.AddComponent( SpriteComponent( self.core.resourceManager.FetchTexture("media\\circle.png") ) )
         self.AddComponent( RigidBody_Circular(20, (random.randint(200, appSize.x-200), (random.randint(-400, -100))) ) )
-        
-    def Step(self):
-        if self.position.y > appSize.y+100:
-            self.core.entityManager.AddEntity( Stone() )
-            self.core.entityManager.RemoveEntity(self)
 
 class Ground(ComponentEntity):
     def Build(self):
@@ -32,7 +28,7 @@ class Ground(ComponentEntity):
 
 # PROGRAM BEGINS
 
-app = GameCore(False, "Crates Demo", appSize.x, appSize.y)
+app = GameCore(False, "Crates Demo", appSize.x, appSize.y, False)
 
 app.renderer.window.framerate_limit = 60
 
