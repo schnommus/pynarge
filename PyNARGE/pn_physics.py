4d9f11a7657e3b56651a7aef9db0ac9b42338379
Sebastian Holzapfel <schnommus@gmail.com>
2013-07-31 21:18:56 +1000

    Added mouse-joint debugging to engine, added respawning to 'crates demo'

--- PyNARGE/pn_physics.py
+++ PyNARGE/pn_physics.py
@@ -5,6 +5,7 @@ from pn_component import Component
 class PhysicsWorld(object):
     def __init__(self, core):
         self.core = core
+        self.mouseJoint = None
     
     def Initialize(self, physicsScale=32):
         self.world = b2World(gravity=(0,-10))
@@ -15,7 +16,36 @@ class PhysicsWorld(object):
     def Update( self ):
         self.world.Step(self.timeStep, self.vel_iters, self.pos_iters)
         self.world.ClearForces()
+        self.Debug_ProcessMouseJoints()
 
+    def Debug_ProcessMouseJoints( self ):
+        mouse = self.core.input.mouse
+        p = self.GlobalToWorld( self.core.input.GetMousePosition() )
+        
+        if mouse.is_button_pressed( mouse.LEFT ):
+            if self.mouseJoint != None:
+                self.mouseJoint.target = p
+                return
+
+            aabb = b2AABB(lowerBound=p-(0.001, 0.001), upperBound=p+(0.001, 0.001))
+
+            query = fwQueryCallback(p)
+            self.world.QueryAABB(query, aabb)
+            
+            if query.fixture:
+                body = query.fixture.body
+                self.mouseJoint = self.world.CreateMouseJoint(
+                        bodyA=self.world.CreateStaticBody(),
+                        bodyB=body, 
+                        target=p,
+                        maxForce=1000.0*body.mass)
+                body.awake = True
+
+        else:
+            if self.mouseJoint:
+                self.world.DestroyJoint(self.mouseJoint)
+                self.mouseJoint = None
+        
     def GetWindowSize(self):
         return self.size
 
@@ -31,15 +61,36 @@ class PhysicsWorld(object):
     def GlobalToWorld(self, coords):
         return Vec2((float(coords.x)-self.core.renderer.window.size.x/2)/self.physicsScale, (self.core.renderer.window.size.y/2-float(coords.y))/self.physicsScale)
 
+
+# Used for mouse joint debugging
+class fwQueryCallback(b2QueryCallback):
+    def __init__(self, p): 
+        super(fwQueryCallback, self).__init__()
+        self.point = p
+        self.fixture = None
+
+    def ReportFixture(self, fixture):
+        body = fixture.body
+        if body.type == b2_dynamicBody:
+            inside=fixture.TestPoint(self.point)
+            if inside:
+                self.fixture=fixture
+                # We found the object, so stop the query
+                return False
+        # Continue the query
+        return True
+
+# BEGIN BODY DEFINITIONS
+
 class StaticBody_Rectangular(Component):
     def __init__( self, size, position=(0,0) ):
         self.size = Vec2(size)
         self.position = Vec2(position)
         
     def Init(self):
+        self.entity.position = self.position
         self.size = self.core.physicsWorld.LocalToWorld(self.size)
         self.position = self.core.physicsWorld.GlobalToWorld(self.position)
-        print self.size, self.position
         b=self.core.physicsWorld.world.CreateStaticBody( position=self.position, shapes=b2PolygonShape(box=self.size) )
 
 class RigidBody_Rectangular(Component):
@@ -58,6 +109,9 @@ class RigidBody_Rectangular(Component):
         self.entity.rotation = 180*self.body.angle/3.1416
         self.entity.position = self.core.physicsWorld.GlobalToScreen(self.body.position)
 
+    def Destroy(self):
+        self.core.physicsWorld.world.DestroyBody(self.body)
+
 class RigidBody_Circular(Component):
     def __init__( self, radius, position=(0,0) ):
         self.radius = float(radius)
@@ -73,3 +127,6 @@ class RigidBody_Circular(Component):
     def Step(self):
         self.entity.rotation = 180*self.body.angle/3.1416
         self.entity.position = self.core.physicsWorld.GlobalToScreen(self.body.position)
+
+    def Destroy(self):
+        self.core.physicsWorld.world.DestroyBody(self.body)
--- example_crates.py
+++ example_crates.py
@@ -2,28 +2,37 @@ from PyNARGE import *
 import random
 import math
 
+appSize = Vec2(800, 600)
+
 class Crate(ComponentEntity):
     def Build(self):
         self.AddComponent( SpriteComponent( self.core.resourceManager.FetchTexture("media\\crate.png") ) )
-        self.AddComponent( RigidBody_Rectangular((16, 16), (random.randint(1,800), (random.randint(1, 400))) ) )
+        self.AddComponent( RigidBody_Rectangular((16, 16), (random.randint(200,appSize.x-200), (random.randint(-400, -100))) ) )
+
+    def Step(self):
+        if self.position.y > appSize.y+100: # If object is offscreen
+            self.core.entityManager.AddEntity( Crate() ) # Spawn new object
+            self.core.entityManager.RemoveEntity(self) # Destroy self
 
 class Stone(ComponentEntity):
     def Build(self):
         self.AddComponent( SpriteComponent( self.core.resourceManager.FetchTexture("media\\circle.png") ) )
-        self.AddComponent( RigidBody_Circular(20, (random.randint(1, 800), (random.randint(1, 400))) ) )
+        self.AddComponent( RigidBody_Circular(20, (random.randint(200, appSize.x-200), (random.randint(-400, -100))) ) )
+        
+    def Step(self):
+        if self.position.y > appSize.y+100:
+            self.core.entityManager.AddEntity( Stone() )
+            self.core.entityManager.RemoveEntity(self)
 
 class Ground(ComponentEntity):
     def Build(self):
-        self.AddComponent( StaticBody_Rectangular( Vec2(300, 10), Vec2(400, 500) ) )
+        self.AddComponent( StaticBody_Rectangular( Vec2(300, 10), Vec2(appSize.x/2, appSize.y-100) ) )
         self.AddComponent( SpriteComponent( self.core.resourceManager.FetchTexture("media\\bar.png") ) )
-
-    def Init(self):
-        self.position = Vec2(400, 500)
 
 
 # PROGRAM BEGINS
 
-app = GameCore(False, "Crates Demo")
+app = GameCore(False, "Crates Demo", appSize.x, appSize.y)
 
 app.renderer.window.framerate_limit = 60
 
