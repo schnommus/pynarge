0f044fda16c8979b01246b47d0a452da7b86e959
Sebastian Holzapfel <schnommus@gmail.com>
2013-07-31 20:17:19 +1000

    Resized physics objects in 'crates demo'

--- example_crates.py
+++ example_crates.py
@@ -5,16 +5,16 @@ import math
 class Crate(ComponentEntity):
     def Build(self):
         self.AddComponent( SpriteComponent( self.core.resourceManager.FetchTexture("media\\crate.png") ) )
-        self.AddComponent( RigidBody_Rectangular((32, 32), (random.randint(1,800), (random.randint(1, 400))) ) )
+        self.AddComponent( RigidBody_Rectangular((16, 16), (random.randint(1,800), (random.randint(1, 400))) ) )
 
 class Stone(ComponentEntity):
     def Build(self):
         self.AddComponent( SpriteComponent( self.core.resourceManager.FetchTexture("media\\circle.png") ) )
-        self.AddComponent( RigidBody_Circular(40, (random.randint(1, 800), (random.randint(1, 400))) ) )
+        self.AddComponent( RigidBody_Circular(20, (random.randint(1, 800), (random.randint(1, 400))) ) )
 
 class Ground(ComponentEntity):
     def Build(self):
-        self.AddComponent( StaticBody_Rectangular( Vec2(300, 25), Vec2(400, 500) ) )
+        self.AddComponent( StaticBody_Rectangular( Vec2(300, 10), Vec2(400, 500) ) )
         self.AddComponent( SpriteComponent( self.core.resourceManager.FetchTexture("media\\bar.png") ) )
 
     def Init(self):
@@ -23,13 +23,13 @@ class Ground(ComponentEntity):
 
 # PROGRAM BEGINS
 
-app = GameCore(True, "Crates Demo")
+app = GameCore(False, "Crates Demo")
 
 app.renderer.window.framerate_limit = 60
 
 app.entityManager.AddEntity( Ground() )
 
-for i in range(50):
+for i in range(100):
     app.entityManager.AddEntity( Crate() )
     app.entityManager.AddEntity( Stone() )
     
