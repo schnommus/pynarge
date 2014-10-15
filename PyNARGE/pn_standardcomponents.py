3905b3cb8a2e6314dbfaa88f407eca804517b0ac
Sebastian Holzapfel <schnommus@gmail.com>
2014-10-16 09:03:13 +1100

    Refactored out StaticPixelSprite; no longer needed; parameterized in PixelSprite

--- PyNARGE/pn_standardcomponents.py
+++ PyNARGE/pn_standardcomponents.py
@@ -72,85 +72,41 @@ class SpriteComponent(Component):
         self.core.renderer.Draw(self.sprite, self.shaderPass)
 
 class PixelSpriteComponent(Component):
-    def __init__(self, texture, forcedSize=None, shaderPass=None, offset=(0,0)):
-        self.sprite = Sprite(texture, Rectangle( (0, 0), (texture.size.x/8, texture.size.y)) )
+    def __init__(self, texture, shaderPass=None, hasDirectionSheet=False):
+        self.sprite = Sprite(texture, Rectangle( (0, 0), (texture.size.x/(8 if hasDirectionSheet else 1), texture.size.y)) )
         self.shaderPass = shaderPass
-        self.offset = Vec2(offset)
         self.alpha = 255
         self.scale = 4
-        self.forcedSize = None
-        if forcedSize != None:
-            self.forcedSize = Vec2(forcedSize)
-            
-        self.sprite.origin = Vec2(texture.size.x/8, texture.size.y)/2
-    
-    def SetTexture(self, texture, forcedSize=None, shaderPass=None):
-        self.sprite = Sprite(texture)
-        self.shaderPass = shaderPass
+        self.hasDirectionSheet = hasDirectionSheet
         
-        self.forcedSize = Vec2(forcedSize)
+        self.sprite.origin = Vec2(texture.size.x/(8 if hasDirectionSheet else 1), texture.size.y)/2
+    
+    def SetTexture(self, texture, shaderPass=None, hasDirectionSheet=False):
+        self.sprite = Sprite(texture, Rectangle( (0, 0), (texture.size.x/(8 if hasDirectionSheet else 1), texture.size.y)) )
+        self.shaderPass = shaderPass
+        self.hasDirectionSheet = hasDirectionSheet
             
-        self.sprite.origin = Vec2(texture.size)/2
+        self.sprite.origin = Vec2(texture.size.x/(8 if hasDirectionSheet else 1), texture.size.y)/2
     
     def Draw(self):
         self.sprite.color = Color(255, 255, 255, self.alpha)
         
         
         self.sprite.ratio = Vec2(self.scale, self.scale)
-        if self.forcedSize != None:
-            self.sprite.scale(
-                ( float(self.forcedSize.x)/float(self.sprite.texture.size.x),
-                float(self.forcedSize.y)/float(self.sprite.texture.size.y) ) )
             
-        self.sprite.position = Vec2(1+4*int(Vec2(self.entity.position + self.offset).x/4), 4*int(Vec2(self.entity.position + self.offset).y/4))
+        self.sprite.position = Vec2(1+4*int(self.entity.position.x/4), 4*int(self.entity.position.y/4))
         self.sprite.rotation = -self.entity.rotation + 22.5
         self.sprite.rotation %= 360
-        index = int( self.sprite.rotation/45)
-        #self.sprite.rotation = int( self.sprite.rotation/45)*45
-        self.sprite.rotation = 0
 
-        self.sprite.texture_rectangle = Rectangle( (index*self.sprite.texture.size.x/8, 0), (self.sprite.texture.size.x/8, self.sprite.texture.size.y))
-        
-        if hasattr( self.entity, 'image' ):
-            self.sprite.rotation = self.entity.rotation
+        if self.hasDirectionSheet:
+            index = int( self.sprite.rotation/45)
+            self.sprite.rotation = 0
+            self.sprite.texture_rectangle = Rectangle( (index*self.sprite.texture.size.x/8, 0), (self.sprite.texture.size.x/8, self.sprite.texture.size.y))
+        else:
+            self.sprite.rotation = int( self.sprite.rotation/90)*90
+
         self.core.renderer.Draw(self.sprite, self.shaderPass)
 
-class StaticPixelSpriteComponent(Component):
-    def __init__(self, texture, forcedSize=None, shaderPass=None, offset=(0,0)):
-        self.sprite = Sprite( texture )
-        self.shaderPass = shaderPass
-        self.offset = Vec2(offset)
-        self.alpha = 255
-        self.scale = 1
-        self.forcedSize = None
-        if forcedSize != None:
-            self.forcedSize = Vec2(forcedSize)
-            
-        self.sprite.origin = Vec2(texture.size)/2
-    
-    def SetTexture(self, texture, forcedSize=None, shaderPass=None):
-        self.sprite = Sprite(texture)
-        self.shaderPass = shaderPass
-        
-        self.forcedSize = Vec2(forcedSize)
-            
-        self.sprite.origin = Vec2(texture.size)/2
-    
-    def Draw(self):
-        self.sprite.color = Color(255, 255, 255, self.alpha)
-        
-        
-        self.sprite.ratio = Vec2(self.scale, self.scale)
-        if self.forcedSize != None:
-            self.sprite.scale(
-                ( float(self.forcedSize.x)/float(self.sprite.texture.size.x),
-                float(self.forcedSize.y)/float(self.sprite.texture.size.y) ) )
-            
-        self.sprite.position = Vec2(1+4*int(Vec2(self.entity.position + self.offset).x/4), 4*int(Vec2(self.entity.position + self.offset).y/4-1))
-        self.sprite.rotation = -self.entity.rotation
-        if hasattr( self.entity, 'image' ):
-            self.sprite.rotation = self.entity.rotation
-        self.core.renderer.Draw(self.sprite, self.shaderPass)
 
 class FollowComponent(Component):
     def __init__(self, target=None, offset=Vec2(0,0)):
